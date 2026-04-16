5d5c75a58d041184ff607dbbfe5215cb6451ca8c
AaronB22 <aaronthesnowboarder@live.com>
2026-04-16 12:05:58 -0700

    test all password lengths

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
 
-  @Test
-  public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
+  
+  @Test 
+  public void checksIfPasswordLengthReturnsCorrect(){
+    PasswordChecker plength= new PasswordChecker(6, 12);
+
+    String actualMed= plength.describePasswordLength("aaronB");
+    String actualLong= plength.describePasswordLength("ThisIsALongPassword");
+    String actualShort = plength.describePasswordLength("short");
+
+    assertEquals("short", actualShort);
+    assertEquals("medium", actualMed);
+    assertEquals("long", actualLong);
+
   }
 
 }
